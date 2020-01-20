e444310c3d8c41b4d142bcb87ed76ef413416398
Trond <56018387+tronderiklarsen@users.noreply.github.com>
2020-01-20 13:25:22 +0100

    Finished level 1 and 2

---
+++ level-1.js
@@ -0,0 +1,106 @@
+// question 1
+
+// var firstNovel = "Carrie";
+
+// question 2
+
+/*
+var person = {
+    name: "Stephen King", 
+    age: 72
+}
+*/
+
+//question 3
+
+/*
+var outOfStock = true;
+
+if (outOfStock) {
+    console.log("In stock");
+}
+else {
+    console.log("Out of stock");
+}
+*/
+
+//question 4
+
+/*
+var numArray = [4, 8, 15, 16, 23];
+
+for (var i = 0; i < numArray.length; i++) {
+    console.log(numArray[i]);
+}
+*/
+
+//question 5
+
+/*
+for (count = 15; count <= 25; count++) {
+    console.log(count);
+}
+*/
+
+// question 6
+
+/*
+for (count = 15; count <= 25; count++) {
+    if (count === 20) {
+        console.log(count);
+    }
+};
+*/
+
+// question 7
+
+/*
+var prisonerIndex = [
+    {
+        name: "Andy Dufresne",
+        inmateNumber: 37927,
+        isAlive: true,
+    },
+    {
+        name: "Ellis Boyd Redding",
+        inmateNumber: 30265,
+        isAlive: true,
+    }
+];
+
+for (var i = 0; i < prisonerIndex.length; i++) {
+    console.log(prisonerIndex[i].inmateNumber)
+    console.log(prisonerIndex[i].isAlive)
+}
+*/
+
+//question 8
+
+/*
+function whatIDontLike(phobia) {
+    console.log("I don't like " + phobia)
+}
+
+whatIDontLike("heights");
+*/
+
+// question 9
+
+/*
+function subtractNumbers(number1, number2) {
+    var sum = number1 - number2;
+    console.log(sum);
+}
+*/
+
+// question 10
+
+/*
+var emptyArray = [];
+
+function addToArray(addThis) {
+    emptyArray.push(addThis);
+}
+
+addToArray("not empty")
+*/
---
+++ level-2.js
@@ -0,0 +1,25 @@
+// question 1
+
+/*
+for (count = 15; count <= 25; count++) {
+    if (count % 2 === 0) {
+    console.log(count);
+    }
+}
+*/
+
+// question 2
+
+/*
+function inception() {
+    console.log("I am a function");
+}
+
+var innerFunction = inception;
+
+function outerFunction(dream) {
+    dream();
+}
+
+outerFunction(innerFunction);
+*/
